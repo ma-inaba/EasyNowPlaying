f44d4ba6f043ed78bd1d1cf02b8fdefd6c70b911
inaba masaya <inabamasaya0506@gmail.com>
2015-11-28 01:01:55 +0900

    First Commit
    UIを暫定的に作成

---
+++ EasyNowPlaying/EasyNowPlaying/Controller/PlaybackViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaybackViewController.h
+//  EasyNowPlaying
+//
+//  Created by inaba masaya on 2015/11/27.
+//  Copyright © 2015年 inaba masaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaybackViewController : UIViewController
+
+@end
