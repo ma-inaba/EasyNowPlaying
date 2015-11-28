98a148fdb95c90e0a789bb9c9f6b34dfbc939617
inaba masaya <inabamasaya0506@gmail.com>
2015-11-28 18:32:49 +0900

    ツイッターアイコン角丸処理の修正
    より滑らかに角丸になるように修正

--- EasyNowPlaying/EasyNowPlaying/View/ArtworkAndTwitterDataView.m
+++ EasyNowPlaying/EasyNowPlaying/View/ArtworkAndTwitterDataView.m
@@ -20,10 +20,19 @@
 
 - (void)reloadViewData {
     
-    // TODO: コード整備
+    // twitterアイコンを使用する
     UIImage *image = [UIImage imageNamed:@"image"];
-    self.twitterIconImageView.image = [image imageWithCornerRadius:120];
+    image = [image imageWithCornerRadius:120];
+
     self.twitterIDLabel.textColor = [ModelLocator sharedInstance].playbackViewModel.musicDataEntity.primaryTextColor;
+
+    self.twitterIconImageView.layer.cornerRadius = self.twitterIconImageView.frame.size.width * 0.5f;
+    self.twitterIconImageView.clipsToBounds = YES;
+    self.twitterIconImageView.image = image;
+    //枠線
+    self.twitterIconImageView.layer.borderWidth = 3.0f;
+    //枠線の色
+    self.twitterIconImageView.layer.borderColor = [[ModelLocator sharedInstance].playbackViewModel.musicDataEntity.backgroundColor CGColor];
 }
 
 @end
