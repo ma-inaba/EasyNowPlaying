5c45b25fd4c2718f4a464d199a97ef291620de5e
inaba masaya <inabamasaya0506@gmail.com>
2015-11-30 22:22:53 +0900

    細かな修正

---
+++ EasyNowPlaying/EasyNowPlaying/Controller/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  EasyNowPlaying
+//
+//  Created by inaba masaya on 2015/11/30.
+//  Copyright © 2015年 inaba masaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
